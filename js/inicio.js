9f4a09f68a392f6de817ab929903c7ff8101bc36
Maricifu <152333294+Maricifu@users.noreply.github.com>
2024-03-12 02:17:32 -0500

    inicio finished

--- js/inicio.js
+++ js/inicio.js
@@ -1,6 +1,6 @@
 // Definición de rutas y contenido de cada sección
 const routes = {
-    home: `<img src="/resources/uds.png" style="width: 38vw; height: auto; margin-bottom: 50px;"><p>En la UDS estamos comprometidos con el desarrllo integral de nuestros estudiantes.</p>`,
+    home: `<img src="/resources/uds.png" style="width: 38vw; height: auto; margin-bottom: 50px;"><p style="font-size: 1.3rem;">En la UDS estamos comprometidos con el desarrllo integral de nuestros estudiantes.</p><img src="/resources/coding.svg">`,
 };
 
 // Función para cargar el contenido de la ruta especificada en el contenedor principal
